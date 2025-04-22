0ed2f45928d98fc438142824b3b66ecacfdaab1c
Sean Murphy <sean@iamseanmurphy.com>
2025-04-22 05:17:16 +0000

    6.7

--- woo-min-max-quantity-step-control-single/wcmmq.php
+++ woo-min-max-quantity-step-control-single/wcmmq.php
@@ -8,9 +8,9 @@
  * Author URI: https://codeastrology.com
  * Tags: WooCommerce, minimum quantity, maximum quantity, woocommrce quantity, input step control for WC, customize wc quantity, wc qt, max qt, min qt, maximum qt, minimum qt
  * 
- * Version: 6.8
+ * Version: 6.7
  * Requires at least:    4.0.0
- * Tested up to:         6.7.2
+ * Tested up to:         6.8
  * WC requires at least: 3.0.0
  * WC tested up to: 	 9.6.2
  * 
